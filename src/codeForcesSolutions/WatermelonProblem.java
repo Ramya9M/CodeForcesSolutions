c008adb67e127d5a6679b5f91bdcd218321d3cd9
Ramya9M <mramya.tvm00@gmail.com>
2023-10-30 20:50:04 +0530

    WatermelonProblem

---
+++ src/codeForcesSolutions/WatermelonProblem.java
@@ -0,0 +1,18 @@
+package codeForcesSolutions;
+
+import java.util.Scanner;
+
+public class WatermelonProblem {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int weight = scanner.nextInt();
+
+        if ((weight > 2) && (weight % 2) == 0)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+    }
+}
